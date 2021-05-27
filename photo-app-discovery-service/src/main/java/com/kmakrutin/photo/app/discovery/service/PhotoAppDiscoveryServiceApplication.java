77bfef81324c6585d191ba573fda5910c30bd84a
kmakrutin <kirill.makrutin@touchnet.com>
2021-05-27 12:38:24 +0300

    Added Eureka Discovery

---
+++ photo-app-discovery-service/src/main/java/com/kmakrutin/photo/app/discovery/service/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.kmakrutin.photo.app.discovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication
+{
+
+  public static void main( String[] args )
+  {
+    SpringApplication.run( PhotoAppDiscoveryServiceApplication.class, args );
+  }
+
+}
---
+++ photo-app-discovery-service/src/test/java/com/kmakrutin/photo/app/discovery/service/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kmakrutin.photo.app.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
