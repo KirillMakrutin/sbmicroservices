8ce13174631a937cb1c6e5bef1d1a5e73bec618a
kmakrutin <kirill.makrutin@touchnet.com>
2021-05-27 13:08:30 +0300

    Users WS

---
+++ photo-app-api-users/src/main/java/com/kmakrutin/photo/app/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,17 @@
+package com.kmakrutin.photo.app.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class PhotoAppApiUsersApplication
+{
+
+  public static void main( String[] args )
+  {
+    SpringApplication.run( PhotoAppApiUsersApplication.class, args );
+  }
+
+}
---
+++ photo-app-api-users/src/test/java/com/kmakrutin/photo/app/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kmakrutin.photo.app.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
