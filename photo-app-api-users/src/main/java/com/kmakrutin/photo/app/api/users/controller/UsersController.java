3a7d4a4ad6504ff195784b4f708b7b03dbe51b6f
kmakrutin <kirill.makrutin@touchnet.com>
2021-05-28 13:23:28 +0300

    API Gateway

--- photo-app-api-users/src/main/java/com/kmakrutin/photo/app/api/users/controller/UsersController.java
+++ photo-app-api-users/src/main/java/com/kmakrutin/photo/app/api/users/controller/UsersController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-public class UserController
+public class UsersController
 {
 
     @GetMapping("/status/check")
