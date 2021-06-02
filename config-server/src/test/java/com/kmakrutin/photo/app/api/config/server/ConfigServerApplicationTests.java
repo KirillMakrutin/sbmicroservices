bdd2ea130f7823bb8a859330318763c889ff3891
kmakrutin <kirill.makrutin@touchnet.com>
2021-06-02 19:11:56 +0300

    Spring Config Server

---
+++ config-server/src/test/java/com/kmakrutin/photo/app/api/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmakrutin.photo.app.api.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
