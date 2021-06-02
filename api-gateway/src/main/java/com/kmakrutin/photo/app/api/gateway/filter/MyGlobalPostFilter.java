7ab9e4af02e778b7894d03c650dc6b148400b798
kmakrutin <kirill.makrutin@touchnet.com>
2021-06-02 12:56:07 +0300

    API Gateway: added global post filter

---
+++ api-gateway/src/main/java/com/kmakrutin/photo/app/api/gateway/filter/MyGlobalPostFilter.java
@@ -0,0 +1,22 @@
+package com.kmakrutin.photo.app.api.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyGlobalPostFilter implements GlobalFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyGlobalPostFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange)
+                .then(Mono.fromRunnable(() -> {
+                    LOGGER.info("My global post filter works...");
+                }));
+    }
+}
