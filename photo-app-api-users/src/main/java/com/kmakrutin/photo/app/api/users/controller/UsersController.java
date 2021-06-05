a9187892eee906f003f62fb0196a47548de54068
Kirill Makrutin <k.makrutin@gmail.com>
2021-06-05 13:03:27 +0300

    JMS

--- photo-app-api-users/src/main/java/com/kmakrutin/photo/app/api/users/controller/UsersController.java
+++ photo-app-api-users/src/main/java/com/kmakrutin/photo/app/api/users/controller/UsersController.java
@@ -27,7 +27,7 @@ public class UsersController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working on port " + environment.getProperty("local.server.port");
+        return "Working on port " + environment.getProperty("local.server.port") + ". Token: " + environment.getProperty("token.secret");
     }
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
