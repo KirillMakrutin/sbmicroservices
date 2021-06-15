a9e0532607cfce5d51606c6f1da620349915d727
kmakrutin <kirill.makrutin@touchnet.com>
2021-06-15 12:21:05 +0300

    Added albums-ws

---
+++ albums/src/main/java/com/kmakrutin/photo/app/api/albums/AlbumsApplication.java
@@ -0,0 +1,15 @@
+package com.kmakrutin.photo.app.api.albums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumsApplication.class, args);
+    }
+
+}
